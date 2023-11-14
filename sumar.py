73609205cf67f6b1fe7f7b1e352b93431362a97a
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2023-11-14 16:17:27 +0100

    Update sumar.py

--- sumar.py
+++ sumar.py
@@ -2,9 +2,9 @@ def sumar_numeros(a, b):
     return a + b
 
 # Ejemplo de variables con dos números
-numero1 = 5
-numero2 = 10
+NUMERO_1 = 5
+NUMERO_2 = 10
 
 # Mostrar la suma usando la función y print
-resultado = sumar_numeros(numero1, numero2)
+RESULTADO = sumar_numeros(numero1, numero2)
 print(f"La suma de {numero1} y {numero2} es: {resultado}")
